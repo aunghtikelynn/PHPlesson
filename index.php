bb403e119b4385bdf00e7c75f86decfeed07d4dc
aunghtikelynn <aunghtikelynn@gmail.com>
2024-09-02 21:32:03 +0630

    php_lesson

--- index.php
+++ index.php
@@ -1,4 +1,27 @@
 <?php 
     echo "Hello World!";
-    echo "<h1>Super Friday.</h1>"
+    echo "<h1>Super Friday.</h1>";
+    $language_type = "Sever-side progamming language";
+    echo $language_type;
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo "<h1>This is paragraph</h1>";
+    ?>
+    <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro quidem natus eius minus neque reiciendis, corrupti, accusamus aut tenetur perspiciatis ducimus quis provident? Voluptas, ipsum error? Voluptatem, voluptatibus nihil? Qui? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius quaerat repellat dignissimos eligendi. Officia incidunt ratione doloribus distinctio laboriosam ipsam architecto. Excepturi tenetur blanditiis voluptatem corporis nisi, ea ut nemo.
+    </p>
+
+    <script>
+        document.write('<h1>I am JavaScript</h1>');
+    </script>
+</body>
+</html>
---
+++ variable_and_string.php
@@ -0,0 +1,16 @@
+<?php
+    //Variable
+    //Naming Rule - No space, No special characters !@#$$*(,), Can't 
+
+    $name = "Aung Htike Lynn";
+    $address = "Pathein";
+
+    //Double quoute and Single Quote
+    echo "My name is $name";
+    echo '<br>';
+    echo 'I live in $address<br>';
+    echo 'I live in '.$address;
+    echo '<br>';
+    echo $name.' Live in '.$address
+
+?>
